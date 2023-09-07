f5a091f30404bec77e6c9b037e8fe53231f38207
SamuTechMan <117929531+SamuTechMan@users.noreply.github.com>
2023-09-06 22:37:29 -0300

    Add files via upload

---
+++ Avaliacao/src/Filme.java
@@ -0,0 +1,101 @@
+public class Filme {
+    private String horarioFilm1, horarioFilm2, horarioFilm3, horarioFilm4;
+    private String generoFilm1, generoFilm2, generoFilm3, generoFilm4;
+    private String duracaoFilm1, duracaoFilm2, duracaoFilm3, duracaoFilm4;
+
+    public String getDuracaoFilm1() {
+        return "180min";
+    }
+
+    public void setDuracaoFilm1(String duracaoFilm1) {
+        this.duracaoFilm1 = duracaoFilm1;
+    }
+
+    public String getDuracaoFilm2() {
+        return "116min";
+    }
+
+    public void setDuracaoFilm2(String duracaoFilm2) {
+        this.duracaoFilm2 = duracaoFilm2;
+    }
+
+    public String getDuracaoFilm3() {
+        return "189min";
+    }
+
+    public void setDuracaoFilm3(String duracaoFilm3) {
+        this.duracaoFilm3 = duracaoFilm3;
+    }
+
+    public String getDuracaoFilm4() {
+        return "125min";
+    }
+
+    public void setDuracaoFilm4(String duracaoFilm4) {
+        this.duracaoFilm4 = duracaoFilm4;
+    }
+
+    public String getGeneroFilm1() {
+        return "Ação/Aventura";
+    }
+
+    public void setGeneroFilm1(String generoFilm1) {
+        this.generoFilm1 = generoFilm1;
+    }
+
+    public String getGeneroFilm2() {
+        return "Ação/Aventura";
+    }
+
+    public void setGeneroFilm2(String generoFilm2) {
+        this.generoFilm2 = generoFilm2;
+    }
+
+    public String getGeneroFilm3() {
+        return "Ação/Aventura";
+    }
+
+    public void setGeneroFilm3(String generoFilm3) {
+        this.generoFilm3 = generoFilm3;
+    }
+
+    public String getGeneroFilm4() {
+        return "Ação/Aventura";
+    }
+
+    public void setGeneroFilm4(String generoFilm4) {
+        this.generoFilm4 = generoFilm4;
+    }
+
+    public String getHorarioFilm1() {
+        return "13:00h";
+    }
+
+    public void setHorarioFilm1(String horarioFilm1) {
+        this.horarioFilm1 = horarioFilm1;
+    }
+
+    public String getHorarioFilm2() {
+        return "15:00h";
+    }
+
+    public void setHorarioFilm2(String horarioFilm2) {
+        this.horarioFilm2 = horarioFilm2;
+    }
+
+    public String getHorarioFilm3() {
+        return "17:00h";
+    }
+
+    public void setHorarioFilm3(String horarioFilm3) {
+        this.horarioFilm3 = horarioFilm3;
+    }
+
+    public String getHorarioFilm4() {
+        return "19:00h";
+    }
+
+    public void setHorarioFilm4(String horarioFilm4) {
+        this.horarioFilm4 = horarioFilm4;
+    }
+}
